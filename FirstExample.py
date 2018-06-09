53976400777bc2b490128c6dedd16fb2a05a9a28
John Corley <john.corley@spanishpoint.ie>
2018-06-09 17:20:33 +0100

    First go

---
+++ FirstExample.py
@@ -0,0 +1,35 @@
+Garage = "Tesla", "Lexus", "Bike"
+
+for each_car in Garage:
+    print(each_car)
+
+
+print('He said \"Hello There\"')
+
+print(4**4)
+
+count = 0
+
+while count < 10:
+    print("The count is ",count)
+    count += 1
+
+for y in range(1,100,7):
+    print ("Y is :",y)
+
+count=int(input("Enter Count"))
+
+if count < y:
+    print ("Count is less than Y \n")
+elif count > y:
+    print ("Count is greater than Y \n")
+else:
+    print ("must be equal")
+
+
+def MySquare(num):
+    answer = num ** num
+    return(answer)
+    
+print(MySquare(10))
+
